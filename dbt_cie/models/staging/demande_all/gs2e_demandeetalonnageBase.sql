b6c4d13443cf35b227915b32c216377b5b9d1e25
ADOU Kouamé Mathurin <adoumathurin@Fodjokra.local>
2025-08-28 12:28:56 +0000

    dbt projet

---
+++ dbt_cie/models/staging/demande_all/gs2e_demandeetalonnageBase.sql
@@ -0,0 +1,21 @@
+{{ config(
+    materialized='table',
+    tags=['staging', 'cie'],
+    partition_by=['partition_id'],
+    file_format='parquet'
+) }}
+
+with source_data as (
+    select * 
+    from {{ var('schema_src') }}.{{ this.name }} t
+    where t.createdon is not null
+),
+
+transformed as (
+    select
+        date_trunc('month', t.createdon) as partition_id,
+        {{ handle_varbinary_columns(this.name) }}
+    from source_data as t
+)
+
+select * from transformed
