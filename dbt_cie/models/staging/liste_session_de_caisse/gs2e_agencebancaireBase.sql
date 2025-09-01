7508c68ce91161cda54a73f958b11e756ced2961
ADOU Kouamé Mathurin <adoumathurin@Fodjokra.local>
2025-09-01 09:51:49 +0000

    dbt projet

--- dbt_cie/models/staging/liste_session_de_caisse/gs2e_agencebancaireBase.sql
+++ dbt_cie/models/staging/liste_session_de_caisse/gs2e_agencebancaireBase.sql
@@ -1,27 +1,5 @@
--- {{ config(
---     materialized='table',
---     tags=['staging', 'cie'],
---     partition_by=['partition_id'],
---     file_format='parquet'
--- ) }}
-
--- with source_data as (
---     select * 
---     from {{ var('schema_src') }}.{{ this.name }} t
---     where t.createdon is not null
--- ),
-
--- transformed as (
---     select
---         date_trunc('month', t.createdon) as partition_id,
---         {{ handle_varbinary_columns(this.name) }}
---     from source_data as t
--- )
-
--- select * from transformed
-
 {{ config(
-    materialized='incremental',
+    materialized='table',
     tags=['staging', 'cie'],
     partition_by=['partition_id'],
     file_format='parquet'
@@ -31,9 +9,6 @@ with source_data as (
     select * 
     from {{ var('schema_src') }}.{{ this.name }} t
     where t.createdon is not null
-    {% if is_incremental() %}
-      and t.createdon > (select max(partition_id) from {{ this }})
-    {% endif %}
 ),
 
 transformed as (
@@ -44,4 +19,3 @@ transformed as (
 )
 
 select * from transformed
-
